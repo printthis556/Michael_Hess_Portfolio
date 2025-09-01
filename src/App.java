adefa535df0e78f89704e585d3fd4b2da6e828db
Michael Hess <mhess0308@eagle.fgcu.edu>
2025-09-01 18:23:00 +0000

    added a lot

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+    
+    // This method is for demonstration; it cannot be called from HTML directly.
+    public static void sayHello() {
+        System.out.println("Hello, user!");
+    }
+}
